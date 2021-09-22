2274de62f6f2baef7823e3b9173cde96772d10cc
Shailesh Kumar <shaileshkumar@Shaileshs-Mac.local>
2021-09-22 07:13:59 +0530

    upto video 20

--- src/test/java/com/tmb/tests/OrangeHRMTests.java
+++ src/test/java/com/tmb/tests/OrangeHRMTests.java
@@ -1,23 +1,34 @@
 package com.tmb.tests;
 
-import com.tmb.pages.OrangeHRMHomePage;
 import com.tmb.pages.OrangeHRMLoginPage;
 import org.assertj.core.api.Assertions;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public final class OrangeHRMTests extends BaseTest {
     private OrangeHRMTests() {
-
     }
 
-    @Test
-    public void loginLoginTest() {
+    @Test(dataProvider = "LoginTestDataProvider")
+    public void loginLogoutTest(String username, String password) {
         //OrangeHRMLoginPage ohlp = new OrangeHRMLoginPage();
-        String title = new OrangeHRMLoginPage().enterUserName("Admin")
-                .enterPassword("admin123").clickLogin()
+        String title = new OrangeHRMLoginPage().enterUserName(username)
+                .enterPassword(password).clickLogin()
                 .clickWelcome().clickLogout().getTitle();
 
         Assertions.assertThat(title)
                 .isEqualTo("OrangeHRM");
     }
+
+    @DataProvider(name = "LoginTestDataProvider",parallel = true)
+    public Object[][] getData() {
+
+        return new Object[][]{
+                {"Admin", "admin123"},
+                {"Admin123", "admin123"},
+                {"Admin", "admin123"},
+                {"Admin123", "admin123"}
+        };
+    }
+
 }
